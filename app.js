ab98a8bcccea76ea1d4b6d601625e018c9750fe9
OMAR ALMUGHAWISH <almgoshomar10@gmail.com>
2025-11-07 10:10:19 +0300

    first edit for app.js

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ app.use((req, res, next) => {
 //      next();
 //  })
 // routes
+//this is the first edit 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/reviews', reviewRouter);
